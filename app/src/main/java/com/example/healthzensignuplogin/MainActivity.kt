60618c3d5c736bcb55b64b0920f65d95d1886e4d
Lea <112546991+t2hale00@users.noreply.github.com>
2024-04-02 11:02:06 +0300

    Edited bug

--- app/src/main/java/com/example/healthzensignuplogin/MainActivity.kt
+++ app/src/main/java/com/example/healthzensignuplogin/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.healthzensignuplogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.ProfileFragment
 import com.example.SettingsFragment
@@ -10,6 +13,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
 
     private  lateinit var bottomNavigationView: BottomNavigationView
+    //private lateinit var educationButton: Button
+    //private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +40,9 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
-        replaceFragment(HomeFragment())
+        if (savedInstanceState == null) {
+            replaceFragment(HomeFragment())
+        }
     }
     private fun replaceFragment(fragment: Fragment){
         supportFragmentManager.beginTransaction().replace(R.id.frame_container,fragment).commit()
