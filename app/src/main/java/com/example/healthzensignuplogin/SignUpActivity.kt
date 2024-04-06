b83418132aef59d94402610317a2945e4d434905
“ShujuanXu” <t2xush00@students.oamk.fi>
2024-04-06 19:37:48 +0300

    not yet done

--- app/src/main/java/com/example/healthzensignuplogin/SignUpActivity.kt
+++ app/src/main/java/com/example/healthzensignuplogin/SignUpActivity.kt
@@ -1,5 +1,6 @@
 package com.example.healthzensignuplogin
 
+import android.app.Activity
 import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
@@ -8,8 +9,15 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.firestore.FirebaseFirestore
 
 class SignUpActivity : AppCompatActivity() {
@@ -22,6 +30,7 @@ class SignUpActivity : AppCompatActivity() {
     private lateinit var signupButton: Button
     private lateinit var loginRedirectText: TextView
     private lateinit var firestore: FirebaseFirestore
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +44,8 @@ class SignUpActivity : AppCompatActivity() {
         signupPassword = findViewById(R.id.signup_password)
         signupButton = findViewById(R.id.signup_button)
         loginRedirectText = findViewById(R.id.loginRedirectText)
+
+
 
         signupButton.setOnClickListener {
 
@@ -78,8 +89,55 @@ class SignUpActivity : AppCompatActivity() {
             }
         }
 
+        val gso= GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient= GoogleSignIn.getClient(this,gso)
+
+        findViewById<Button>(R.id.btnGoogleSignIn).setOnClickListener {
+            googleSignIn()
+        }
+
         loginRedirectText.setOnClickListener {
             startActivity(Intent(this@SignUpActivity, LogInActivity::class.java))
         }
+    }
+
+    private fun googleSignIn() {
+
+        val signInClient=googleSignInClient.signInIntent
+        launcher.launch(signInClient)
+
+    }
+    private val launcher=registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+        if(result.resultCode== Activity.RESULT_OK){
+            val task=GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            manageResults(task)
+        }
+    }
+
+    private fun manageResults(task: Task<GoogleSignInAccount>) {
+        val account: GoogleSignInAccount?=task.result
+        if(account!=null){
+            val credential= GoogleAuthProvider.getCredential(account.idToken,null)
+            auth.signInWithCredential(credential).addOnCompleteListener {
+                if(task.isSuccessful){
+                    val intent=Intent(this,MainActivity::class.java)
+                    startActivity(intent)
+
+                    Toast.makeText(this,"Account created",Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(this, task.exception.toString(),Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        else{
+            Toast.makeText(this, task.exception.toString(),Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
