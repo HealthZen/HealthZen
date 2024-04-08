48c8967a3958811a83ab849feb340c9169bfd5e7
“ShujuanXu” <t2xush00@students.oamk.fi>
2024-04-08 21:29:07 +0300

    made bottom bar in community page

--- app/src/main/java/com/example/healthzensignuplogin/CommunityActivity.kt
+++ app/src/main/java/com/example/healthzensignuplogin/CommunityActivity.kt
@@ -4,13 +4,42 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.example.ProfileFragment
+import com.example.SettingsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class CommunityActivity : AppCompatActivity() {
-
+    private  lateinit var bottomNavigationView: BottomNavigationView
     private lateinit var goPostButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_community)
+        bottomNavigationView=findViewById(R.id.bottom_navigation)
+
+
+
+
+        bottomNavigationView.setOnItemSelectedListener { menuItem->
+            when (menuItem.itemId){
+                R.id.bottom_home->{
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.bottom_profile->{
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+                R.id.bottom_settings->{
+                    replaceFragment(SettingsFragment())
+                    true
+                }
+                else ->false
+            }
+
+        }
+
+
 
 
         goPostButton=findViewById(R.id.goPostButton)
@@ -21,6 +50,8 @@ class CommunityActivity : AppCompatActivity() {
         }
     }
 
-
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container,fragment).commit()
+    }
 
 }
