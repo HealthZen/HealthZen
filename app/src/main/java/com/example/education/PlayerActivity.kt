17699b68bf183acbb3f5f92761446f6df366265c
Lea <112546991+t2hale00@users.noreply.github.com>
2024-04-11 09:29:59 +0300

    Fixed bug. Video now playing.

--- app/src/main/java/com/example/education/PlayerActivity.kt
+++ app/src/main/java/com/example/education/PlayerActivity.kt
@@ -12,9 +12,19 @@ class PlayerActivity : AppCompatActivity() {
         setContentView(R.layout.activity_player)
 
         val webView = findViewById<WebView>(R.id.web_view)
-        val video =
-            "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/tUesv5u5bvA\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>"
-        webView.loadData(video, "text/html", "utf-8")
+
+        // Get the video ID from the intent extra
+        val videoId = intent.getStringExtra("video_id")
+
+        // Generate the YouTube embed URL using the video ID
+        val videoUrl = "https://www.youtube.com/embed/$videoId"
+
+        // Load the generated URL into the WebView
+        webView.loadData(
+            "<iframe width=\"100%\" height=\"100%\" src=\"$videoUrl\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>",
+            "text/html",
+            "utf-8"
+        )
         webView.settings.javaScriptEnabled = true
         webView.webChromeClient = WebChromeClient()
     }
