63694300781be37e2bd0ab9b59501c6cda4cb6b3
Lea <112546991+t2hale00@users.noreply.github.com>
2024-04-13 20:26:26 +0300

    Added stress relief branch

---
+++ app/src/main/java/com/example/stressrelief/PlayerActivitySR.kt
@@ -0,0 +1,31 @@
+package com.example.stressrelief
+
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.healthzensignuplogin.R
+
+class PlayerActivitySR : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+
+        val webView = findViewById<WebView>(R.id.web_view)
+
+        // Get the video ID from the intent extra
+        val videoId = intent.getStringExtra("video_id")
+
+        // Generate the YouTube embed URL using the video ID
+        val videoUrl = "https://www.youtube.com/embed/$videoId"
+
+        // Load the generated URL into the WebView
+        webView.loadData(
+            "<iframe width=\"100%\" height=\"100%\" src=\"$videoUrl\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>",
+            "text/html",
+            "utf-8"
+        )
+        webView.settings.javaScriptEnabled = true
+        webView.webChromeClient = WebChromeClient()
+    }
+}
--- app/src/main/java/com/example/stressrelief/StressReliefActivity.kt
+++ app/src/main/java/com/example/stressrelief/StressReliefActivity.kt
@@ -2,7 +2,13 @@ package com.example.stressrelief
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.healthzensignuplogin.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class StressReliefActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,3 +16,4 @@ class StressReliefActivity : AppCompatActivity() {
         setContentView(R.layout.activity_stress_relief)
     }
 }
+
